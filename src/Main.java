c73a9e7ad69707fd770c1a810bd6ce3302d976fe
KatrinAn <anpipinakaka@gmail.com>
2023-01-26 22:46:22 +0200

    added class user and created users

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        User user1 = new User("Ірина","Вітовська", 14, "05", 1998,"test@gmail.com","3807778866",66,"120/80", 7000);
+      user1.secondName = "Романченко";
+      User user2 = new User("Максим", "Коцюбинський", 14, "07", 1986, "user2@gmail.com", "380667755000", 89, "120/60", 12000 );
+      user2.steps = 20000;
+      User user3 = new User("Олена","Пчілка", 30, "11", 1991,"user3@gmail.com", "380675588822", 60,"110/50", 25000);
+      user3.weight= 55;
+      User user4 = new User("Іван", "Шевченко", 25, "03", 1970, "user4@gmail.com", "380504499922", 90, "130/70", 6000);
+
+        User[] users = {user1, user2, user3,user4};
+        for (User u : users){
+            u.printAccountInfo();
+        }
+
 
     }
 }
