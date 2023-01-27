9502c36d0ff930fd525eecb7563dde0b98869582
KatrinAn <anpipinakaka@gmail.com>
2023-01-27 22:30:14 +0200

    public canged to private, and modified as per comments

--- src/User.java
+++ src/User.java
@@ -1,73 +1,59 @@
 public class User {
-    private String firstName;
-
-    public int getYearOfBirth() {
-        return yearOfBirth;
-    }
-
-    public void setYearOfBirth(int yearOfBirth) {
-        this.yearOfBirth = yearOfBirth;
-    }
-
-    public String getMonthOfBirth() {
-        return monthOfBirth;
-    }
-
-    public void setMonthOfBirth(String monthOfBirth) {
-        this.monthOfBirth = monthOfBirth;
-    }
-
-    public int getDayOfBirth() {
-        return dayOfBirth;
-    }
-
-    public void setDayOfBirth(int dayOfBirth) {
-        this.dayOfBirth = dayOfBirth;
-    }
-
-    private int yearOfBirth;
-    private String monthOfBirth;
-    private int dayOfBirth;
-      private String email;
-    private String phoneNumber;
-
-    public int getAge() {
-        int age = 2020 -yearOfBirth;
-        return age;
-    }
-
-    private int age;
-
+     int age;
+     private String firstName;
     public String getFirstName() {
         return firstName;
     }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    private int yearOfBirth;
+    public int getYearOfBirth() {
+        return yearOfBirth;
     }
-
+    private String monthOfBirth;
+    public String getMonthOfBirth() {
+        return monthOfBirth;
+    }
+    private int dayOfBirth;
+    public int getDayOfBirth() {
+        return dayOfBirth;
+    }
+    private String email;
     public String getEmail() {
         return email;
     }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
+    private String phoneNumber;
     public String getPhoneNumber() {
         return phoneNumber;
     }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+    private int weight;
+    public int getWeight() {
+        return weight;
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+    private String pressure;
+    public String getPressure() {
+        return pressure;
+    }
+    public void setPressure(String pressure) {
+        this.pressure = pressure;
+    }
+    private int steps;
+    public int getSteps() {
+        return steps;
+    }
+    public void setSteps(int steps) {
+        this.steps = steps;
+    }
+    private String secondName;
+    public String getSecondName() {
+        return secondName;
+    }
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
     }
 
-    public int weight;
-    public String pressure;
-    public int steps;
-    public String secondName;
-
-    public User(String firstName,String secondName, int dayOfBirth, String monthOfBirth, int yearOfBirth, String email, String phoneNumber, int weight, String pressure, int steps) {
+    public  User(String firstName, String secondName, int dayOfBirth, String monthOfBirth, int yearOfBirth, String email, String phoneNumber, int weight, String pressure, int steps) {
         this.firstName = firstName;
         this.dayOfBirth= dayOfBirth;
         this.monthOfBirth= monthOfBirth;
@@ -78,10 +64,12 @@ public class User {
         this.pressure = pressure;
         this.steps = steps;
         this.secondName = secondName;
+            }
+    public int getAge() {
+        int age = 2020 -yearOfBirth;
+         return age;
     }
         public void printAccountInfo(){
         System.out.println("Персональні дані " + firstName +" "+ secondName + ", дата народження: "+ dayOfBirth+"."+monthOfBirth+"."+ yearOfBirth+", вік:"+ getAge() +", вага: "+ weight+", тиск: "+ pressure+", зроблено кроків: "+steps+", електрона пошта: "+ email+", номер телефону: "+phoneNumber);
     }
-
-
     }
